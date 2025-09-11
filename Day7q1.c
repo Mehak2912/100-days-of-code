76228578d7e276c66b59b770d5606a366329e352
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2025-09-11 22:43:41 +0530

    Create Day7q1.c

---
+++ Day7q1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int year;
+    printf("enter year:");
+    scanf("%d", &year);
+    if(year % 400==0){
+        printf("it is a leap year");
+    }
+    else if(year % 100==0){
+        printf("it is not leap year");
+    } 
+    else if(year%4==0){
+        printf("it is a leap year");
+    }
+    else{
+        printf("it is not leap year");
+    }   
+
+    return 0;
+}
