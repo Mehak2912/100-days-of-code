c08cc540e475efe4b04ad6a0a92062228e211a6b
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2025-09-12 19:25:27 +0530

    Create Day8q1.c

---
+++ Day8q1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>
+int main() {
+    char ch;
+    printf("enter ch:");
+    scanf("%c", &ch);
+    if(isupper(ch)){
+        printf("'%c', is an uppercase alphabet.\n", ch);
+    }
+    else if (islower(ch)) {
+        printf("'%c', is a lowercase alphabet.\n", ch);
+    }
+    else if (isdigit(ch)){
+        printf("'%c', is a digit .\n", ch);
+    }
+    else{
+        printf("'%c', is a special character.\n", ch);
+    }   
+
+    return 0;
+}
